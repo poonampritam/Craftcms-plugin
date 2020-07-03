522a2b771a547fd13528d479594e1b5fc266ffcf
webtoolbox <webtoolbox@3648292.no-reply.drupal.org>
2020-07-03 14:35:42 +0530

    Update Websitetoolboxforums.php

--- src/Websitetoolboxforums.php
+++ src/Websitetoolboxforums.php
@@ -1,155 +1,80 @@
 <?php
 /**
- * websitetoolboxforums plugin for Craft CMS 3.x
+ * Vanillaforums plugin for Craft CMS 3.x
  *
- * test plugin
+ * Single Sign On plugin for VanillaForums/jsConnect and CraftCMS
  *
- * @link      http://websitetoolbox.com
- * @copyright Copyright (c) 2020 Website Toolbox
+ * @link      https://websitetoolbox.com/
+ * @copyright Copyright (c) 2019 websitetoolbox
  */
 
 namespace websitetoolbox\websitetoolboxforums;
 
-use websitetoolbox\websitetoolboxforums\fields\WebsitetoolboxforumsField as WebsitetoolboxforumsFieldField;
+use websitetoolbox\websitetoolboxforums\services\Sso as SsoService;
+use websitetoolbox\websitetoolboxforums\variables\VanillaforumsVariable;
+use websitetoolbox\websitetoolboxforums\models\Settings;
 
 use Craft;
 use craft\base\Plugin;
-use craft\services\Plugins;
-use craft\events\PluginEvent;
-use craft\web\UrlManager;
-use craft\services\Fields;
-use craft\events\RegisterComponentTypesEvent;
-use craft\events\RegisterUrlRulesEvent;
+use craft\web\twig\variables\CraftVariable;
 
 use yii\base\Event;
 
 /**
- * Craft plugins are very much like little applications in and of themselves. We’ve made
- * it as simple as we can, but the training wheels are off. A little prior knowledge is
- * going to be required to write a plugin.
+ * Class Vanillaforums
  *
- * For the purposes of the plugin docs, we’re going to assume that you know PHP and SQL,
- * as well as some semi-advanced concepts like object-oriented programming and PHP namespaces.
+ * @author    websitetoolbox
+ * @package   Vanillaforums
+ * @since     3.0.0
  *
- * https://docs.craftcms.com/v3/extend/
- *
- * @author    Website Toolbox
- * @package   Websitetoolboxforums
- * @since     1.0.0
- *
+ * @property  SsoService $sso
  */
-class Websitetoolboxforums extends Plugin
+class Vanillaforums extends Plugin
 {
     // Static Properties
     // =========================================================================
 
     /**
-     * Static property that is an instance of this plugin class so that it can be accessed via
-     * Websitetoolboxforums::$plugin
-     *
-     * @var Websitetoolboxforums
+     * @var Vanillaforums
      */
     public static $plugin;
+
+    /**
+     * @var bool
+     */
+    public static $craft31 = false;
 
     // Public Properties
     // =========================================================================
 
     /**
-     * To execute your plugin’s migrations, you’ll need to increase its schema version.
-     *
      * @var string
      */
     public $schemaVersion = '1.0.0';
-
-    /**
-     * Set to `true` if the plugin should have a settings view in the control panel.
-     *
-     * @var bool
-     */
-    public $hasCpSettings = false;
-
-    /**
-     * Set to `true` if the plugin should have its own section (main nav item) in the control panel.
-     *
-     * @var bool
-     */
-    public $hasCpSection = false;
 
     // Public Methods
     // =========================================================================
 
     /**
-     * Set our $plugin static property to this class so that it can be accessed via
-     * Websitetoolboxforums::$plugin
-     *
-     * Called after the plugin class is instantiated; do any one-time initialization
-     * here such as hooks and events.
-     *
-     * If you have a '/vendor/autoload.php' file, it will be loaded for you automatically;
-     * you do not need to load it in your init() method.
-     *
+     * @inheritdoc
      */
     public function init()
     {
         parent::init();
         self::$plugin = $this;
 
-        // Register our site routes
+        self::$craft31 = version_compare(Craft::$app->getVersion(), '3.1', '>=');
+
         Event::on(
-            UrlManager::class,
-            UrlManager::EVENT_REGISTER_SITE_URL_RULES,
-            function (RegisterUrlRulesEvent $event) {
-                $event->rules['siteActionTrigger1'] = 'websitetoolboxforums/default';
+            CraftVariable::class,
+            CraftVariable::EVENT_INIT,
+            function (Event $event) {
+                /** @var CraftVariable $variable */
+                $variable = $event->sender;
+                $variable->set('websitetoolboxforums', VanillaforumsVariable::class);
             }
         );
 
-        // Register our CP routes
-        Event::on(
-            UrlManager::class,
-            UrlManager::EVENT_REGISTER_CP_URL_RULES,
-            function (RegisterUrlRulesEvent $event) {
-                $event->rules['cpActionTrigger1'] = 'websitetoolboxforums/default/do-something';
-            }
-        );
-
-        // Register our fields
-        Event::on(
-            Fields::class,
-            Fields::EVENT_REGISTER_FIELD_TYPES,
-            function (RegisterComponentTypesEvent $event) {
-                $event->types[] = WebsitetoolboxforumsFieldField::class;
-            }
-        );
-
-        // Do something after we're installed
-        Event::on(
-            Plugins::class,
-            Plugins::EVENT_AFTER_INSTALL_PLUGIN,
-            function (PluginEvent $event) {
-                if ($event->plugin === $this) {
-                    // We were just installed
-                }
-            }
-        );
-
-/**
- * Logging in Craft involves using one of the following methods:
- *
- * Craft::trace(): record a message to trace how a piece of code runs. This is mainly for development use.
- * Craft::info(): record a message that conveys some useful information.
- * Craft::warning(): record a warning message that indicates something unexpected has happened.
- * Craft::error(): record a fatal error that should be investigated as soon as possible.
- *
- * Unless `devMode` is on, only Craft::warning() & Craft::error() will log to `craft/storage/logs/web.log`
- *
- * It's recommended that you pass in the magic constant `__METHOD__` as the second parameter, which sets
- * the category to the method (prefixed with the fully qualified class name) where the constant appears.
- *
- * To enable the Yii debug toolbar, go to your user account in the AdminCP and check the
- * [] Show the debug toolbar on the front end & [] Show the debug toolbar on the Control Panel
- *
- * http://www.yiiframework.com/doc-2.0/guide-runtime-logging.html
- */
         Craft::info(
             Craft::t(
                 'websitetoolboxforums',
@@ -163,4 +88,29 @@ class Websitetoolboxforums extends Plugin
     // Protected Methods
     // =========================================================================
 
+    /**
+     * @inheritdoc
+     */
+    protected function createSettingsModel()
+    {
+        return new Settings();
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function settingsHtml(): string
+    {
+        // Set up the form controls for editing the connection.
+        $hashTypes = hash_algos();
+        $hashTypes = array_combine($hashTypes, $hashTypes);
+
+        return Craft::$app->view->renderTemplate(
+            'websitetoolboxforums/settings',
+            [
+                'settings' => $this->getSettings(),
+                'hashTypes' => $hashTypes,
+            ]
+        );
+    }
 }
