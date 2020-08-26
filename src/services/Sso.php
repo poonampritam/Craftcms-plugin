13429d0c23e2abd7c0e252947093194aa537347a
poonampritam <65331985+poonampritam@users.noreply.github.com>
2020-08-26 18:31:36 +0530

    Add files via upload

--- src/services/Sso.php
+++ src/services/Sso.php
@@ -16,9 +16,9 @@ use websitetoolbox\websitetoolboxforums\models\SsoData;
 
 use Craft;
 use craft\base\Component;
-echo "vvvvvvvvvvvvvvv";exit;
- 
-/** @noinspection MissingPropertyAnnotationsInspection */
+use craft\web\View;
+use craft\services\Config;
+ /** @noinspection MissingPropertyAnnotationsInspection */
 
 /**
  * @author    websitetoolbox
@@ -27,153 +27,138 @@ echo "vvvvvvvvvvvvvvv";exit;
  */
 class Sso extends Component
 {
-    // Constants
-    // =========================================================================
+    
+    function afterLogin(){  
+       $token = Craft::$app->getSession()->get(Craft::$app->getUser()->tokenParam); 
+        if($token){ 
+             $forumApiKey = Craft::$app->getProjectConfig()->get('plugins.websitetoolboxforums.settings.forumApiKey');
+             $forumUrl = Craft::$app->getProjectConfig()->get('plugins.websitetoolboxforums.settings.forumUrl');
+            if($forumApiKey){ 
+                $myUserQuery = \craft\elements\User::find();
+                $userEmail = Craft::$app->getUser()->getIdentity()->email;
+                $userId= Craft::$app->getUser()->getIdentity()->id;
+                $userName= Craft::$app->getUser()->getIdentity()->username;
+                $RequestUrl = $forumUrl."/register/setauthtoken";
+                $postData = array('type'=>'json','apikey' => $forumApiKey, 'user' => $userName,'email'=>$userEmail,'externalUserid'=>$userId);
+                 $ch = curl_init();
+                curl_setopt($ch,CURLOPT_URL,$RequestUrl);
+                curl_setopt($ch, CURLOPT_POST, 1);
+                curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+                curl_setopt($ch,CURLOPT_POSTFIELDS,json_encode($postData));
+                curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+                'Content-Type: application/json','Accept: application/json'));      
+                curl_setopt($ch, CURLOPT_HEADER, 0);
+                $response = curl_exec($ch);
+                $result = json_decode($response, true); 
+                setcookie("forumLogoutToken", $result['authtoken'], time() + 3600,"/");
+                setcookie("forumLoginUserid", $result['userid'], time() + 3600,"/");
 
-    /**
-     * @event SsoDataEvent The event that is triggered before the SSO data is used,
-     * you may modify the [[SsoDataEvent::data]] as you see fit. You may set
-     * [[SsoDataEvent::isValid]] to `false` to prevent SSO data from being used.
-     *
-     * ```php
-     * use websitetoolbox\websitetoolboxforums\services\Sso;
-     * use websitetoolbox\websitetoolboxforums\events\SsoDataEvent;
-     *
-     * Event::on(Sso::class,
-     *     SsoDataEvent::EVENT_SSO_DATA,
-     *     function(SsoDataEvent $event) {
-     *         // potentially set $event->isValid or modify $event->data
-     *     }
-     * );
-     * ```
-     */
-    const EVENT_SSO_DATA = 'websitetoolboxForumsSsoData';
-
-    // Public Methods
-    // =========================================================================
-
-    /**
-     * Generate the jsConnect string for single sign on
-     *
-     * @param int $userId
-     *
-     * @return string
-     * @throws \yii\base\ExitException
-     */
-    public function output(int $userId = 0): string
-    { echo "vvvvvvvvvv";exit;
-        $result = '';
-        $settings = $this->getPluginSettings();
-        $ssoData = $this->getSsoData($userId);
-        if ($ssoData !== null) {
-            $request = Craft::$app->getRequest();
-            //ob_start(); // Start output buffering
-            \WriteJsConnect(
-                $ssoData->toArray(),
-                $request->get(),
-                $settings->forumUsername,
-                $settings->forumPassword,
-                $settings->forumEmbedded,
-               
-            );
-            //$result = ob_get_contents();
-            //ob_end_clean(); // Store buffer in variable
-        }
-
-        Craft::$app->end();
-        //return $result === false ? '' : $result;
-    }
-
-    /**
-     * Generate an SSO string suitable for passing in the url for embedded SSO
-     *
-     * @param int $userId
-     *
-     * @return string
-     */
-    public function embeddedOutput(int $userId = 0): string
-    {
-        $result = '';
-        $settings = $this->getPluginSettings();
-        $ssoData = $this->getSsoData($userId);
-        if ($ssoData !== null) {
-            $result = \JsSSOString(
-                $ssoData->toArray(),
-                $settings->forumUsername,
-                $settings->forumPassword,
-                $settings->forumEmbedded
-            );
-        }
-
-        return $result;
-    }
-
-    // Private Methods
-    // =========================================================================
-
-    /**
-     * Return an SSOData object filled in with the current user's info, or null
-     *
-     * @param int $userId
-     *
-     * @return SsoData|null
-     */
-    private function getSsoData(int $userId = 0)
-    {
-        $data = null;
-
-        // Assume the currently logged in user if no $userId is passed in
-        if ($userId === 0) {
-            $user = Craft::$app->getUser()->getIdentity();
-        } else {
-            $users = Craft::$app->getUsers();
-            $user = $users->getUserById($userId);
-        }
-        if ($user) {
-            $generalConfig = Craft::$app->getConfig()->getGeneral();
-            $name = $generalConfig->useEmailAsUsername ? $user->getFullName() : $user->username;
-            $photoUrl = '';
-            $photo = $user->getPhoto();
-            if ($photo !== null) {
-                $photoUrl = $photo->getUrl();
+                
             }
-            // Fill in the initial data
-            $data = new SsoData([
-                'uniqueid' => $user->id,
-                'name' => $name,
-                'email' => $user->email,
-                'photourl' => $photoUrl,
-            ]);
         }
-        // Give plugins a chance to modify it
-        $event = new SsoDataEvent([
-            'user' => $user,
-            'data' => $data,
-        ]);
-        $this->trigger(self::EVENT_SSO_DATA, $event);
-        if (!$event->isValid) {
-            return null;
+         
+     }
+    function afterUserCreate($userId){ 
+       echo $forumUrl = Craft::$app->getProjectConfig()->get('plugins.websitetoolboxforums.settings.forumUrl',false);
+       echo $forumApiKey = Craft::$app->getProjectConfig()->get('plugins.websitetoolboxforums.settings.forumApiKey',false); 
+        $postData = array( 'type'=>'json','apikey'=>$forumApiKey,'member' => $_POST['username'],
+        'externalUserid' => $userId, 
+        'email' => $_POST['email']);
+        if(isset($_POST['firstName'])){
+           $postData['name'] =  $_POST['firstName'];
         }
-
-        return $data;
+        if(isset($_POST['firstName'])){
+           $postData['name'] .=  " ".$_POST['lastName'];
+        }
+        
+        $RequestUrl = $forumUrl . "/register/create_account/";
+        $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL,$RequestUrl);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,json_encode($postData));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+        'Content-Type: application/json','Accept: application/json'));      
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $response = curl_exec($ch); 
+        $result = json_decode($response, true);
     }
+  function afterUpdateUser(){
+  //Websitetoolboxforums::getInstance()->sso->updateUserDetails();  
+  $emailToVerify  = $_SESSION['userEmailBeforeUpdate'];
+  $userId     = Websitetoolboxforums::getInstance()->sso->getUserid($emailToVerify);
+  $userName = $_POST['username'];
+  $externalUserid = $_POST['userId'];
+  $email = $_POST['email'];;
+  $userDetails = array("type"=>"json","email" => $email,
+          "username" => $userName,
+          "externalUserid" => $externalUserid,
+          "name" => $userName);
 
-    /**
-     * Get the plugin's settings, parsing any environment variables
-     *
-     * @return Settings
-     */
-    private function getPluginSettings(): Settings
-    {
-        /** @var Settings $settings */
-        $settings = Websitetoolboxforums::$plugin->getSettings();
-        if (Websitetoolboxforums::$craft31) {
-            $settings->forumUsername = Craft::parseEnv($settings->forumUsername);
-            $settings->forumPassword = Craft::parseEnv($settings->forumPassword);
-            $settings->forumEmbedded = Craft::parseEnv($settings->forumEmbedded);
-             
+  $url =  "/users/$userId";
+  $response = Websitetoolboxforums::getInstance()->sso->sendApiRequest('POST',$url,$userDetails);
+ }
+function getUserid($userEmail){
+     if ($userEmail) {
+        $data     = array(
+            "email" => $userEmail
+        );
+        $response = Websitetoolboxforums::getInstance()->sso->sendApiRequest('GET', "/users/", $data); 
+        if ($response->{'data'}[0]->{'userId'}) {
+             return $response->{'data'}[0]->{'userId'};
         }
+    }
+}
+function sendApiRequest($method, $path, $data){
+    $forumApiKey = Craft::$app->getProjectConfig()->get('plugins.websitetoolboxforums.settings.forumApiKey',false);
+    $url = "https://api.websitetoolbox.com/dev/api" . $path;
+    if ($method == "GET") {
+        echo $url = sprintf("%s?%s", $url, http_build_query($data));
+    }
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+        "x-api-key: " . $forumApiKey,
+        'Content-Type: application/json'
+    ));
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    if ($method == "POST") {
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($data));
+    } else if ($method == "GET") {
+        curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+    }
+    $response = curl_exec($curl);
+    curl_close($curl);
+    return json_decode($response);
+}
+    function afterDeleteUser($userName){        
+       $forumApiKey = Craft::$app->getProjectConfig()->get('plugins.websitetoolboxforums.settings.forumApiKey',false);
+       $forumUrl = Craft::$app->getProjectConfig()->get('plugins.websitetoolboxforums.settings.forumUrl',false);
+       $postData         = array('type'=>'json','apikey' => $forumApiKey,'massaction' => 'decline_mem','usernames' => $userName);
+       $RequestUrl =  $forumUrl."/register";
+       $ch = curl_init();
+        curl_setopt($ch,CURLOPT_URL,$RequestUrl);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
+        curl_setopt($ch,CURLOPT_POSTFIELDS,json_encode($postData));
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                                                          
+        'Content-Type: application/json','Accept: application/json'));      
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        $response = curl_exec($ch); 
+        $result = json_decode($response, true);
+    }
+    function afterLogOut(){  
+      echo '<img src="https://beta.websitetoolbox.com/register/logout?authtoken='.$_COOKIE['forumLogoutToken'].'" border="0" width="0" height="0" alt="" id="imageTag">'; 
+      
+     
+     }
+     function serviceMethod() 
+    {   
 
-        return $settings;
+        $oldMode = \Craft::$app->view->getTemplateMode();
+\Craft::$app->view->setTemplateMode(View::TEMPLATE_MODE_CP);
+$html = \Craft::$app->view->renderTemplate('websitetoolboxforums/index');
+\Craft::$app->view->setTemplateMode($oldMode);
     }
 }
